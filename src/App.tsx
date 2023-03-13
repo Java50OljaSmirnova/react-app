5aec531db6fc8637cb6f75e7660e0275ee3b137c
oljas <olja.smirnov93@gmail.com>
2023-03-13 19:23:16 +0200

    HW #32 solution four timers

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,21 @@ import './App.css';
 import { Timer } from './components/Timer';
 
 function App() {
-  return <Timer/>;
+  const column : React.CSSProperties = {display: "flex", flexDirection: "column", 
+  height: "90vh", justifyContent: "center"};
+  const row : React.CSSProperties = {display: "flex", flexDirection: "row",
+   justifyContent: "space-around"};
+  return <div style={column}>
+    <div style={row}>
+    <Timer cityCountry='Israel'/>
+    <Timer cityCountry='New York'/>
+    </div>
+    <div style={row}>
+    <Timer cityCountry='Yekaterinburg'/>
+    <Timer cityCountry='Japan'/>
+    </div>
+    </div>
+  
 }
 
 export default App;
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,22 +1,37 @@
-import React from "react"
-export const Timer: React.FC = () => {
-const styles: React.CSSProperties = {backgroundColor: "lightblue", fontSize: "2em"};
-const hStylesRed: React.CSSProperties = {color: 'red', fontSize: "2em"}
-const hStylesBlue: React.CSSProperties = {color: 'blue', fontSize: "2em"}
-setTimeout(tic, 1000);
-const timeInterval: number = 10000 ;
-const [time, setTime] = React.useState(new Date());
-function tic() {
-    setTime(new Date())
-}
-const [changer, setChange] = React.useState(hStylesBlue);
-setTimeout(switchStyle, timeInterval);
-function switchStyle(){
-    changer === hStylesBlue ? setChange (hStylesRed) : setChange(hStylesBlue)
+import React, { useState, useEffect, useRef } from "react";
+import timeZones from "../time-zones";
+type Props = {
+    cityCountry: string;
 }
 
+export const Timer: React.FC<Props> = ({ cityCountry }) => {
+    const styles: React.CSSProperties = { backgroundColor: "lightblue", fontSize: "2em" };
+    const timeZone = useRef<string | undefined>();
+    const [time, setTime] = useState(new Date());
+    function tic() {
+        setTime(new Date());
+        console.log("kuku");
+    }
+
+    function getTimeZone(): string | undefined {
+        const index = timeZones.findIndex(tz => JSON.stringify(tz).includes(cityCountry));
+        return index < 0 ? undefined : timeZones[index].name;
+    }
+    useEffect(
+        () => {
+            timeZone.current = getTimeZone();
+        }, [cityCountry]
+    )
+
+    useEffect(() => {
+        const interval = setInterval(tic, 2000);
+        console.log("useEffect")
+        return () => clearInterval(interval);
+    }, [])
+
     return <div>
-        <h2 style={changer}>Current Time</h2>
-        <p style={styles}>{time.toLocaleTimeString()}</p>
+        <h2 >Current Time in {cityCountry}</h2>
+        <p style={styles}>{time.toLocaleTimeString(undefined, 
+            {timeZone : timeZone.current})}</p>
     </div>
 }
