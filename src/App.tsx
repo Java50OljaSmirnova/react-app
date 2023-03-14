1aea2ca74398bf44818e3948eeba297dd727f4a6
oljas <olja.smirnov93@gmail.com>
2023-03-14 18:14:35 +0200

    CW#33

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
 
 import './App.css';
+import { InputTest } from './components/InputTest';
 import { Timer } from './components/Timer';
 
 function App() {
-  const column : React.CSSProperties = {display: "flex", flexDirection: "column", 
-  height: "90vh", justifyContent: "center"};
-  const row : React.CSSProperties = {display: "flex", flexDirection: "row",
-   justifyContent: "space-around"};
-  return <div style={column}>
-    <div style={row}>
-    <Timer cityCountry='Israel'/>
-    <Timer cityCountry='New York'/>
-    </div>
-    <div style={row}>
-    <Timer cityCountry='Yekaterinburg'/>
-    <Timer cityCountry='Japan'/>
-    </div>
-    </div>
-  
+  return <InputTest/>
 }
 
 export default App;
