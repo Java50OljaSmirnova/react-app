7da3b4a48bc8dd37004b82b396527db701e6a55a
oljas <olja.smirnov93@gmail.com>
2023-03-14 19:19:56 +0200

    HW33 input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
 import './App.css';
-import { InputTest } from './components/InputTest';
 import { Timer } from './components/Timer';
 
 function App() {
-  return <InputTest/>
+  const column : React.CSSProperties = {display: "flex", flexDirection: "column", 
+  height: "90vh", justifyContent: "center"};
+  const row : React.CSSProperties = {display: "flex", flexDirection: "row",
+   justifyContent: "space-around"};
+  return <div style={column}>
+    <div style={row}>
+    <Timer cityCountry='Israel'/>
+    <Timer cityCountry='New York'/>
+    </div>
+    <div style={row}>
+    <Timer cityCountry='Yekaterinburg'/>
+    <Timer cityCountry='Japan'/>
+    </div>
+    </div>
+  
 }
 
 export default App;
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
+import { Input } from "./Input";
 import timeZones from "../time-zones";
 type Props = {
     cityCountry: string;
@@ -8,18 +9,19 @@ export const Timer: React.FC<Props> = ({ cityCountry }) => {
     const styles: React.CSSProperties = { backgroundColor: "lightblue", fontSize: "2em" };
     const timeZone = useRef<string | undefined>();
     const [time, setTime] = useState(new Date());
+    const [inputCityCountry, setCityCountry] = useState<string>(cityCountry);
     function tic() {
         setTime(new Date());
         console.log("kuku");
     }
 
-    function getTimeZone(): string | undefined {
-        const index = timeZones.findIndex(tz => JSON.stringify(tz).includes(cityCountry));
+    function getTimeZone(value: string): string | undefined {
+        const index = timeZones.findIndex(tz => JSON.stringify(tz).includes(value));
         return index < 0 ? undefined : timeZones[index].name;
     }
     useEffect(
         () => {
-            timeZone.current = getTimeZone();
+            timeZone.current = getTimeZone(cityCountry);
         }, [cityCountry]
     )
 
@@ -28,10 +30,21 @@ export const Timer: React.FC<Props> = ({ cityCountry }) => {
         console.log("useEffect")
         return () => clearInterval(interval);
     }, [])
-
+function submitFn(inputValeu: string): string{
+    const tempZone = getTimeZone(inputValeu);
+    let res: string = '';
+    if(!tempZone){
+        res = `${inputValeu} doesn't exist in the time zones`
+    } else {
+        timeZone.current = tempZone;
+        setCityCountry(inputValeu);
+    }
+    return res;
+}
     return <div>
-        <h2 >Current Time in {cityCountry}</h2>
-        <p style={styles}>{time.toLocaleTimeString(undefined, 
-            {timeZone : timeZone.current})}</p>
+        <Input submitFn={submitFn} placeHolder = {"Enter city or country"}/>
+        <h2 >Current Time in {inputCityCountry}</h2>
+        <p style={styles}>{time.toLocaleTimeString(undefined,
+            { timeZone: timeZone.current })}</p>
     </div>
 }
