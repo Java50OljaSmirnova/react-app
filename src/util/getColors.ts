db22f81b00d0c8e85e41cc79aa347b8453093a97
oljas <olja.smirnov93@gmail.com>
2023-03-24 10:54:48 +0300

    HW#36 life matrix algorithm emplymentation

--- src/util/getColors.ts
+++
@@ -1,6 +0,0 @@
-export function getColors(): string[] {
-    const colors: string[] = [
-        'red', 'blue', 'yellow', ' green', 'grey', 'lightgray', 'pink', 'orange', 'lightbrown'
-    ]
-    return colors
-}
--- src/util/random.ts
+++ src/util/random.ts
@@ -1,6 +1,22 @@
-export function getRandomNumbers(min: number, max: number): number{
-return min + Math.trunc (Math.random() * (max - min))
+export function getRandomNumber(min: number, max: number): number {
+    return min + Math.trunc(Math.random() * (max - min))
 }
 export function getRandomElement(array: any[]): any {
-    return array[getRandomNumbers(0, array.length)];
+    return array[getRandomNumber(0, array.length)];
+}
+export function getRandomMatrix(rows: number, columns: number, minNumber: number,
+    maxNumber: number): number[][] {
+    function getRow(): number[] {
+        return Array.from(Array(columns)).map(() =>
+            getRandomNumber(minNumber, maxNumber + 1))
+        /*return Array.from({length:columns}, ()=> getRandomNumber(minNumber, maxNumber + 1));
+          this cod is beter*/
+    }
+    const res: number[][] = Array.from(Array(rows)).map(() => getRow());
+    /*  const res: number[][] = Array.from({length: row}, () => getRow()); 
+     this cod is beter */
+    
+
+    return res;
+
 }
